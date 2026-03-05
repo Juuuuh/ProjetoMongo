60c65f9d2a8c29c2f69a9495c06223b331d02418
Juuuuh <ajuliagomes45@gmail.com>
2026-03-05 10:32:44 +0000

    Obter um utilizador por id

--- src/main/java/com/juliagomes/workshopMongo/services/UserService.java
+++ src/main/java/com/juliagomes/workshopMongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.juliagomes.workshopMongo.services;
 
 import com.juliagomes.workshopMongo.domain.User;
 import com.juliagomes.workshopMongo.repository.UserRepository;
+import com.juliagomes.workshopMongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,4 +18,13 @@ public class UserService {
         return repo.findAll();
 
     }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found! :c "));
+    }
+
+
+
+
 }
