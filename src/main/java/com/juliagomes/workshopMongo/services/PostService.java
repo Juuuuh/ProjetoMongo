389291598daec2ae1b2f002829ea63d99147a97e
Juuuuh <ajuliagomes45@gmail.com>
2026-03-05 18:24:06 +0000

    Consulta simples com query methods

--- src/main/java/com/juliagomes/workshopMongo/services/PostService.java
+++ src/main/java/com/juliagomes/workshopMongo/services/PostService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 public class PostService {
     @Autowired
@@ -17,5 +18,9 @@ public class PostService {
     public Post findById(String id){
         Optional<Post> obj = repo.findById(id);
         return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found! :c "));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
